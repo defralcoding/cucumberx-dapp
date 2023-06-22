d57dcd706dc9429eb000299cbd50ee81be7486d3
Alessandro <defranceschi_a@yahoo.com>
2023-06-22 17:02:14 +0200

    Responsive navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -16,55 +16,62 @@ export const Navbar = () => {
 	const isLoggedIn = useGetIsLoggedIn();
 	const { address } = useGetAccount();
 
+	const navbarExpand = "md";
+
 	const handleLogout = () => {
 		logout(`${window.location.origin}/unlock`);
 	};
 
 	return (
-		<BsNavbar className="bg-white border-bottom px-4 py-3">
+		<BsNavbar
+			expand={navbarExpand}
+			className="bg-white border-bottom px-4 py-3"
+		>
 			<div className="container-fluid">
-				<Link
-					className="d-flex align-items-center navbar-brand mr-0"
-					to={isLoggedIn ? routeNames.dashboard : routeNames.home}
-				>
-					<MultiversXLogo className="multiversx-logo" />
-					<span className="dapp-name text-muted">{dAppName}</span>
-				</Link>
+				<BsNavbar.Brand>
+					<Link
+						className="d-flex align-items-center navbar-brand mr-0"
+						to={isLoggedIn ? routeNames.dashboard : routeNames.home}
+					>
+						<MultiversXLogo className="multiversx-logo" />
+						<span className="dapp-name text-muted">{dAppName}</span>
+					</Link>
+				</BsNavbar.Brand>
 
-				<Nav className="ml-auto">
-					{isLoggedIn && (
-						<>
-							{adminAddresses.includes(address) && (
+				<BsNavbar.Toggle aria-controls="responsive-navbar-nav" />
+
+				<BsNavbar.Collapse id="responsive-navbar-nav">
+					<Nav className="ml-auto">
+						{isLoggedIn && (
+							<>
+								{adminAddresses.includes(address) && (
+									<NavItem>
+										<Link
+											to={routeNames.adminSettings}
+											className={
+												"btn btn-primary mb-2 mb-" +
+												navbarExpand +
+												"-0 mr-0 mr-" +
+												navbarExpand +
+												"-2"
+											}
+										>
+											Admin Settings
+										</Link>
+									</NavItem>
+								)}
 								<NavItem>
-									<Link
-										to={routeNames.adminSettings}
-										className="btn btn-primary mr-2"
+									<button
+										className="btn btn-primary"
+										onClick={handleLogout}
 									>
-										Admin Settings
-									</Link>
+										Logout
+									</button>
 								</NavItem>
-							)}
-							{/*
-							<NavItem>
-								<Link to={routeNames.home} className="nav-link">
-									<FontAwesomeIcon
-										icon={faChartSimple}
-										className="text-muted"
-									/>
-								</Link>
-							</NavItem>
-							*/}
-							<NavItem>
-								<button
-									className="btn btn-primary"
-									onClick={handleLogout}
-								>
-									Logout
-								</button>
-							</NavItem>
-						</>
-					)}
-				</Nav>
+							</>
+						)}
+					</Nav>
+				</BsNavbar.Collapse>
 			</div>
 		</BsNavbar>
 	);

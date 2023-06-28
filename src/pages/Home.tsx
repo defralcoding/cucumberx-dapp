1efc4144f18ddbc0a4a32227bab8c3f8e9ba4247
Alessandro <defranceschi_a@yahoo.com>
2023-06-28 11:09:08 +0200

    HOMEPAGE

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,38 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthRedirectWrapper } from "components";
 import { dAppName } from "config";
 import { routeNames } from "routes";
+import { MyApiNetworkProvider } from "helpers/MyApiNetworkProvider";
+import { useGetNetworkConfig } from "hooks";
 import CountUp from "react-countup";
 
 const HomePage = () => {
+	const {
+		network: { apiAddress },
+	} = useGetNetworkConfig();
+	const apiNetworkProvider = new MyApiNetworkProvider(apiAddress);
+
+	const [stakedCount, setStakedCount] = useState(0);
+
+	useEffect(() => {
+		apiNetworkProvider
+			.getAccountNftsCountFromCollection(
+				"erd1qqqqqqqqqqqqqpgqpt97ps7w69ng3ynxpn3lq9fc0wj5u9hddn3qp4lqzu",
+				"CUMBX-762eec"
+			)
+			.then((count) => {
+				setStakedCount(count);
+			});
+	}, []);
+
 	return (
 		<div className="d-flex flex-fill align-items-center container">
 			<div className="row w-100">
 				<div className="col-12 col-md-8 col-lg-5 mx-auto">
 					<div className="card shadow-sm rounded p-4 border-0">
 						<div className="card-body text-center">
-							<h2 className="mb-3">CucumberX {dAppName}</h2>
+							<h2 className="mb-3">CUCUMBERX {dAppName}</h2>
 
 							<p className="mb-3">
 								Login using your MultiversX wallet.
@@ -30,9 +50,9 @@ const HomePage = () => {
 						<p>STAKED CUCUMBERS</p>
 						<p>
 							<CountUp
-								end={100}
+								end={stakedCount}
 								decimals={0}
-								duration={3}
+								duration={2}
 								useEasing={true}
 							/>
 						</p>
